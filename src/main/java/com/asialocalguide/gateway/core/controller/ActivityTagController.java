d4488dfbdb6ba772dbb115736f85021cc821c8ca
Samuel Lee <sam.leefr@gmail.com>
2025-04-05 21:09:06 +0200

    package for ActivityTagDTO

--- src/main/java/com/asialocalguide/gateway/core/controller/ActivityTagController.java
+++ src/main/java/com/asialocalguide/gateway/core/controller/ActivityTagController.java
@@ -1,25 +1,24 @@
 package com.asialocalguide.gateway.core.controller;
 
-import com.asialocalguide.gateway.core.dto.ActivityTagDTO;
+import com.asialocalguide.gateway.core.dto.activitytag.ActivityTagDTO;
 import com.asialocalguide.gateway.core.service.activitytag.ActivityTagService;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/activity-tags")
 public class ActivityTagController {
 
-    private final ActivityTagService activityTagService;
+  private final ActivityTagService activityTagService;
 
-    public ActivityTagController(ActivityTagService activityTagService) {
-        this.activityTagService = activityTagService;
-    }
+  public ActivityTagController(ActivityTagService activityTagService) {
+    this.activityTagService = activityTagService;
+  }
 
-    @GetMapping
-    public List<ActivityTagDTO> getActivityTags() {
-        return activityTagService.getActivityTags();
-    }
+  @GetMapping
+  public List<ActivityTagDTO> getActivityTags() {
+    return activityTagService.getActivityTags();
+  }
 }
--- src/main/java/com/asialocalguide/gateway/core/dto/ActivityTagDTO.java
+++
@@ -1,4 +0,0 @@
-package com.asialocalguide.gateway.core.dto;
-
-public record ActivityTagDTO(Long id, String name, String promptText) {
-}
