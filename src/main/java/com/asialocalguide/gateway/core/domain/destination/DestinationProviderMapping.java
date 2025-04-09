6cfd76e7f27deea682cc0686beb11149df1f1726
Samuel Lee <sam.leefr@gmail.com>
2025-04-09 16:56:19 +0200

    Fix DestinationTranslation

--- src/main/java/com/asialocalguide/gateway/core/domain/destination/DestinationProviderMapping.java
+++ src/main/java/com/asialocalguide/gateway/core/domain/destination/DestinationProviderMapping.java
@@ -4,9 +4,8 @@ import com.asialocalguide.gateway.core.domain.BookingProvider;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-
 import java.util.Objects;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor
@@ -15,48 +14,35 @@ import java.util.Objects;
 @Getter
 public class DestinationProviderMapping {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  // TODO: Use composite keys
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "destination_id", nullable = false)
-    @Setter
-    @NotNull
-    private Destination destination;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "destination_id", nullable = false)
+  @Setter
+  @NotNull
+  private Destination destination;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "provider_id", nullable = false)
-    @Setter
-    @NotNull
-    private BookingProvider provider;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "provider_id", nullable = false)
+  @Setter
+  @NotNull
+  private BookingProvider provider;
 
-    @Setter
-    @NotNull
-    @NotEmpty
-    private String providerDestinationId;
+  @Setter @NotNull @NotEmpty private String providerDestinationId;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    DestinationProviderMapping that = (DestinationProviderMapping) o;
+    return Objects.equals(id, that.id);
+  }
 
-        DestinationProviderMapping mapping = (DestinationProviderMapping) o;
-
-        if (id != null && mapping.id != null) {
-            return Objects.equals(id, mapping.id);
-        }
-
-        return Objects.equals(provider, mapping.provider) &&
-                Objects.equals(providerDestinationId, mapping.providerDestinationId);
-    }
-
-    @Override
-    public int hashCode() {
-        if (id != null) {
-            return id.hashCode();
-        }
-
-        return Objects.hash(provider, providerDestinationId);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 }
