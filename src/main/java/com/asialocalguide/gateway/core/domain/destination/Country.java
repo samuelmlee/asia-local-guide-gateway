1a37b59ae3ad663d28f3027e6327318921bc166e
Samuel Lee <sam.leefr@gmail.com>
2025-04-09 16:06:59 +0200

    Fix Country entities

--- src/main/java/com/asialocalguide/gateway/core/domain/destination/Country.java
+++ src/main/java/com/asialocalguide/gateway/core/domain/destination/Country.java
@@ -1,75 +1,87 @@
 package com.asialocalguide.gateway.core.domain.destination;
 
+import com.google.common.base.Objects;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-
 @Entity
 @NoArgsConstructor
 public class Country implements Translatable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Getter
+  private Long id;
 
-    @OneToMany(mappedBy = "country", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<CountryTranslation> countryTranslations = new HashSet<>();
+  @OneToMany(mappedBy = "country", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<CountryTranslation> countryTranslations = new HashSet<>();
 
-    @Column(name = "iso_2_code")
-    @NotEmpty
-    @Length(min = 2, max = 2)
-    @Getter
-    @Setter
-    private String iso2Code;
+  @Column(name = "iso_2_code")
+  @NotEmpty
+  @Length(min = 2, max = 2)
+  @Getter
+  @Setter
+  private String iso2Code;
 
-    public Country(String iso2Code) {
-        if (iso2Code == null || iso2Code.length() != 2) {
-            throw new IllegalArgumentException("ISO2 code must be 2 characters long");
-        }
-
-        this.iso2Code = iso2Code;
+  public Country(String iso2Code) {
+    if (iso2Code == null || iso2Code.length() != 2) {
+      throw new IllegalArgumentException("ISO2 code must be 2 characters long");
     }
 
-    public void addTranslation(CountryTranslation translation) {
-        if (translation == null) {
-            throw new IllegalArgumentException("Translation cannot be null");
-        }
-        if (countryTranslations == null) {
-            countryTranslations = new HashSet<>();
-        }
-        translation.setCountry(this);
-        countryTranslations.add(translation);
+    this.iso2Code = iso2Code;
+  }
+
+  public void addTranslation(CountryTranslation translation) {
+    if (translation == null) {
+      throw new IllegalArgumentException("Translation cannot be null");
+    }
+    if (countryTranslations == null) {
+      countryTranslations = new HashSet<>();
+    }
+    translation.setCountry(this);
+    countryTranslations.add(translation);
+  }
+
+  public void removeTranslation(CountryTranslation translation) {
+    if (translation == null) {
+      throw new IllegalArgumentException("Translation cannot be null");
     }
 
-    public void removeTranslation(CountryTranslation translation) {
-        if (translation == null) {
-            throw new IllegalArgumentException("Translation cannot be null");
-        }
+    if (countryTranslations == null) {
+      return;
+    }
+    translation.setCountry(null);
+    countryTranslations.remove(translation);
+  }
 
-        if (countryTranslations == null) {
-            return;
-        }
-        translation.setCountry(null);
-        countryTranslations.remove(translation);
+  @Override
+  public Optional<String> getTranslation(LanguageCode languageCode) {
+    if (languageCode == null || countryTranslations == null) {
+      return Optional.empty();
     }
 
-    @Override
-    public Optional<String> getTranslation(LanguageCode languageCode) {
-        if (languageCode == null || countryTranslations == null) {
-            return Optional.empty();
-        }
+    return countryTranslations.stream()
+        .filter(ct -> ct.getId() != null && languageCode.equals(ct.getId().getLanguageCode()))
+        .findFirst()
+        .map(CountryTranslation::getName);
+  }
 
-        return countryTranslations.stream()
-                .filter(ct -> ct.getId().getLanguageCode().equals(languageCode))
-                .findFirst()
-                .map(CountryTranslation::getName);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    Country country = (Country) o;
+    return Objects.equal(id, country.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
 }
--- src/main/java/com/asialocalguide/gateway/core/domain/destination/CountryTranslationId.java
+++ src/main/java/com/asialocalguide/gateway/core/domain/destination/CountryTranslationId.java
@@ -3,35 +3,37 @@ package com.asialocalguide.gateway.core.domain.destination;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.Objects;
 
 @NoArgsConstructor
 @Embeddable
 @Getter
-@Setter
-public class CountryTranslationId {
-    @Column(name = "country_id")
-    private Long countryId;
+public class CountryTranslationId implements Serializable {
+  @Column(name = "country_id")
+  private Long countryId;
 
-    @Column(name = "language_code")
-    @Convert(converter = LanguageCodeConverter.class)
-    private LanguageCode languageCode;
+  @Column(name = "language_code")
+  @Convert(converter = LanguageCodeConverter.class)
+  private LanguageCode languageCode;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CountryTranslationId that = (CountryTranslationId) o;
-        return Objects.equals(countryId, that.countryId) &&
-                Objects.equals(languageCode, that.languageCode);
-    }
+  public CountryTranslationId(LanguageCode languageCode) {
+    // countryId is set by Hibernate with @MapsId in CountryTranslation
+    this.languageCode = languageCode;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(countryId, languageCode);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    CountryTranslationId that = (CountryTranslationId) o;
+    return Objects.equals(countryId, that.countryId) && Objects.equals(languageCode, that.languageCode);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(countryId, languageCode);
+  }
 }
