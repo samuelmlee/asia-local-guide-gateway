dce0dad182414d9bf617aeec3f6c311ec35343e9
Samuel Lee <sam.leefr@gmail.com>
2026-03-16 19:17:47 +0100

    More merge clean up

--- src/main/java/com/asialocalguide/gateway/core/config/AcceptHeaderResolver.java
+++ src/main/java/com/asialocalguide/gateway/core/config/AcceptHeaderResolver.java
@@ -1,20 +1,21 @@
 package com.asialocalguide.gateway.core.config;
 
-import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 public class AcceptHeaderResolver extends AcceptHeaderLocaleResolver {
 
-  List<Locale> locales = Arrays.stream(SupportedLocale.values()).map(l -> Locale.of(l.getCode())).toList();
+	List<Locale> locales = Arrays.stream(SupportedLocale.values()).map(l -> Locale.of(l.getCode())).toList();
 
-  @Override
-  public Locale resolveLocale(HttpServletRequest request) {
-    String headerLang = request.getHeader("Accept-Language");
-    return headerLang == null || headerLang.isEmpty()
-        ? Locale.getDefault()
-        : Locale.lookup(Locale.LanguageRange.parse(headerLang), locales);
-  }
+	@Override
+	public Locale resolveLocale(HttpServletRequest request) {
+		String headerLang = request.getHeader("Accept-Language");
+		return headerLang == null || headerLang.isEmpty() ? Locale.getDefault()
+				: Locale.lookup(Locale.LanguageRange.parse(headerLang), locales);
+	}
 }
